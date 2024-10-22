a90117df8ebc62f5dd693d414f3f02c13c683a58
Renato <renato92.hora@gmail.com>
2024-10-21 21:38:36 -0300

    feat: showing submission motification

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -33,6 +33,7 @@ def index():
         form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully", "success")
 
     return render_template("index.html")
 
